1b7b7b4c907d8a390de0643865b1ef050c070184
fmstyles14@gmail.com <fmstyles14@gmail.com>
2024-10-09 18:25:30 +0100

    uodate cred

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'jenkin-server-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t fmstyles/demo-app:jma-3.2 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push fmstyles/demo-app:jma-3.2'
     }
 }
 
